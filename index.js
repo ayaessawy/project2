0875710a93776d09aef49adce81659e9b625a028
ayaessawy <68233245+ayaessawy@users.noreply.github.com>
2022-06-19 10:02:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var  closee = document.getElementById("close");
+var aktevriacodesection  = document.getElementById("aktevriacode-section")
+var aktevieriabutton   = document.getElementById("aktevieriabutton")
+
+aktevieriabutton.addEventListener("click",function(){
+        aktevriacodesection.style.display="block";
+
+
+          
+
+
+
+});
+closee.addEventListener("click",function(){
+        aktevriacodesection.style.display="none";
+
+})
